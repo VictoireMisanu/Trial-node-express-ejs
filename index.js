9251d20a8bcccb3ec17f8e8fb701c0bf04836d23
Victoire Muvumbi <victoire.muvumbi@iMac-de-kadea-64.local>
2024-06-07 12:32:50 +0100

    server and template created

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const main_app = express()
+const articles = require("./data/db.json")
+
+main_app.set("view engine", "ejs")
+main_app.set("views", __dirname + "/views")
+main_app.use(express.static(__dirname + "/public"))
+
+main_app.get("/", function(req, res){
+    res.render("index")
+});
+
+main_app.get("/about", function(req, res){
+    res.render("about")
+});
+
+main_app.get("/*", function(req, res){
+    res.render("404")
+});
+
+
+
+const port = 3000
+
+main_app.listen(port, function(){
+    console.log(`le serveur est lancé au port ${port} et l'application est disponible sur http://localhost:${port}`)
+});
