ca2e0cab264e7dcbec710c31723c460270c6df80
Vic <vicmisanu63@gmail.com>
2024-06-10 09:56:38 +0200

    from scratch

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const main_app = express()
 const articles = require("./data/db.json")
+// const articles = require("./data/db.json")
+
 
 main_app.set("view engine", "ejs")
 main_app.set("views", __dirname + "/views")
@@ -14,9 +16,24 @@ main_app.get("/about", function(req, res){
     res.render("about")
 });
 
+main_app.get("/articles", function(req, res){
+    res.render("articles")
+})
+
+// main_app.get("/articles:slug", function(req, res){
+//     const {slug} = req.params
+//     const article = articles.find((article) =>{articles.slug === slug})
+//     if(article){
+//         res.render('article', {article})
+//     }
+//     else{
+//         res.render('404')
+//     }
+// })
 main_app.get("/*", function(req, res){
     res.render("404")
 });
+
 
 
 
